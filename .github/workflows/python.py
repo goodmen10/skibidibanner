06a171e2f9144cc9d079b85ea516cb5be825fbed
goodmen10 <156316610+goodmen10@users.noreply.github.com>
2024-01-11 16:14:16 +0100

    modified:   .github/workflows/python.py

--- .github/workflows/python.py
+++ .github/workflows/python.py
@@ -1,6 +1,7 @@
 import json
 import random
 import time
+import array
 
 def pick_random_elements(source_file_path, destination_file_path):
     try:
@@ -10,17 +11,16 @@ def pick_random_elements(source_file_path, destination_file_path):
 
         # Categories to pick from
         categories = ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Mythic']
-
         # Ensure all categories are present
         if all(category in data for category in categories):
             # Randomly pick one item from each category and store them
             selected_items = {category: random.choice(data[category]) for category in categories}
+            selected_items["Time"] = time.time()
         else:
             missing_categories = [category for category in categories if category not in data]
             raise ValueError(f"JSON does not contain required categories: {missing_categories}")
 
         # Write the selected items to the destination JSON file
-        print(time.time())
         with open(destination_file_path, 'w') as file:
             json.dump(selected_items, file, indent=4)
 
